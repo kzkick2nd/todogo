51f02bd50f53fe5735d910fd9e5225c0311eda06
kzkick2nd <aruga.kazuki@gmail.com>
2018-12-02 01:58:10 +0900

    init

---
+++ v1/main.go
@@ -0,0 +1,12 @@
+package main()
+
+func main() {
+	// todogo add TEXT
+	// todogo list
+	// todogo done ID
+	// todogo remove ID
+
+	// use default package
+	// test driven develop
+	// save to file
+}
